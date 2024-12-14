b23846a5b4430bd2d18dd882e1734b4272b69c36
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-12-14 12:39:02 -0800

    Fixed Forge loading issue.

--- forge/src/main/java/com/anthonyhilyard/merchantmarkers/forge/MerchantMarkersForge.java
+++ forge/src/main/java/com/anthonyhilyard/merchantmarkers/forge/MerchantMarkersForge.java
@@ -3,13 +3,13 @@ package com.anthonyhilyard.merchantmarkers.forge;
 import com.anthonyhilyard.merchantmarkers.MerchantMarkers;
 
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MerchantMarkers.MODID)
 public final class MerchantMarkersForge
 {
-	public MerchantMarkersForge(ModLoadingContext context)
+	public MerchantMarkersForge(FMLJavaModLoadingContext context)
 	{
 		context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "ANY", (remote, isServer) -> true));
 	}
